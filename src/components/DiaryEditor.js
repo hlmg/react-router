83b2876dda08a8c1532d802ff3640ec3865e6a34
hlmg <devhlmg@gmail.com>
2024-01-09 14:17:04 +0900

    일기 상세 페이지 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -4,38 +4,8 @@ import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
 import { DiaryDispatchContext } from "../App";
-
-const emotion_list = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "완전 좋음",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "좋음",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁨",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "끔찍함",
-  },
-];
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
@@ -101,7 +71,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
         <section>
           <h4>오늘의 감정</h4>
           <div className={"input_box emotion_list_wrapper"}>
-            {emotion_list.map((it) => (
+            {emotionList.map((it) => (
               <EmotionItem
                 key={it.emotion_id}
                 {...it}
